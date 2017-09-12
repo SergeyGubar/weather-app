90d29665545c110cd9befc6d53cd1d80b87896aa
Gubar Sergey <serhii.hubar@nure.ua>
2017-09-12 14:24:12 +0300

    Adapter + visual fix

---
+++ app/src/main/java/com/example/sergey/weatherapp/WeatherAdapter.kt
@@ -0,0 +1,26 @@
+package layout
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.sergey.weatherapp.R
+
+/**
+ * Created by Sergey on 9/12/2017.
+ */
+class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder> {
+    override fun onBindViewHolder(holder: WeatherViewHolder?, position: Int) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): WeatherViewHolder {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    class WeatherViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
