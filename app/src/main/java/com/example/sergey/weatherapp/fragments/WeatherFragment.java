aa22c646e46cad9d5a93f925754e8fca147adf08
Sergey <sergey.gubar187@gmail.com>
2017-09-14 19:20:44 +0300

    Basic UI display

--- app/src/main/java/com/example/sergey/weatherapp/fragments/WeatherFragment.java
+++ app/src/main/java/com/example/sergey/weatherapp/fragments/WeatherFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.sergey.weatherapp.R;
@@ -19,14 +20,11 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Date;
-import java.util.Objects;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import static android.content.ContentValues.TAG;
-import static com.example.sergey.weatherapp.utilities.WeatherUtilites.fahrenheitToCelcius;
 
 /**
  * Created by Sergey on 9/12/2017.
@@ -34,32 +32,45 @@ import static com.example.sergey.weatherapp.utilities.WeatherUtilites.fahrenheit
 
 public class WeatherFragment extends Fragment {
 
-    private TextView mTemperatureTextView;
-    private OkHttpClient mClient;
     private static final String TAG = "WeatherFragment";
+    private TextView mTemperatureTextView;
+    private TextView mCityTextView;
+    private TextView mSummaryTextView;
+    private ImageView mWeatherImageView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         boolean isAttachedToParent = false;
         View inflatedView = inflater.inflate(R.layout.main_weather_fragment, null, isAttachedToParent);
-        mClient = new OkHttpClient();
         mTemperatureTextView = (TextView) inflatedView.findViewById(R.id.degrees_text_view);
-
+        mCityTextView = (TextView) inflatedView.findViewById(R.id.city_text_view);
+        mSummaryTextView = (TextView) inflatedView.findViewById(R.id.weather_description_text_view);
+        mWeatherImageView = (ImageView) inflatedView.findViewById(R.id.weather_image_view);
         new WeatherTask().execute("https://api.darksky.net/forecast/5b0ccf2ae41ee32686d2ae27eff06011/" +
                 "49.96535591,36.2878418?exclude=hourly,minutely/");
         return inflatedView;
     }
 
+    private void setWeather(Weather weather) {
+        mTemperatureTextView.setText(weather.getTemperature() + "°C");
+        mSummaryTextView.setText(weather.getSummary());
+        mCityTextView.setText("Kharkiv");
+    }
+
+
     public class WeatherTask extends AsyncTask<String, Void, String> {
+        private OkHttpClient mClient;
+        private final String TAG = WeatherTask.class.getSimpleName();
         @Override
         protected String doInBackground(String... params) {
             String queryUrl = params[0];
+            mClient = new OkHttpClient();
             Request request = new Request.Builder()
                     .url(queryUrl)
                     .build();
 
-            Response response = null;
+            Response response;
             try {
                 response = mClient.newCall(request).execute();
                 return response.body().string();
@@ -78,14 +89,14 @@ public class WeatherFragment extends Fragment {
 
                     String parsedDate = currentWeather.getString("time");
                     String parsedSummary = currentWeather.getString("summary");
-                    String parsedCelciusTemp = currentWeather.getString("temperature");
+                    String parsedTemp = currentWeather.getString("temperature");
                     String parsedHumidity = currentWeather.getString("humidity");
                     String parsedPressure = currentWeather.getString("pressure");
 
                     long milliSeconds = Long.valueOf(parsedDate) * 1000;
                     Date date = new Date(milliSeconds);
 
-                    int temp = WeatherUtilites.fahrenheitToCelcius(Double.valueOf(parsedCelciusTemp));
+                    int temp = WeatherUtilites.fahrenheitToCelcius(Double.valueOf(parsedTemp));
 
                     double humidity = Double.valueOf(parsedHumidity);
 
@@ -93,6 +104,8 @@ public class WeatherFragment extends Fragment {
 
                     Weather weather = new Weather(date, parsedSummary, temp, humidity, pressure);
                     Log.d(TAG, weather.toString());
+
+                    setWeather(weather);
 
                 } catch (JSONException e) {
                     Log.e(TAG, "Json parsing failed");
@@ -104,6 +117,5 @@ public class WeatherFragment extends Fragment {
             }
         }
     }
-
 
 }
--- app/src/main/java/com/example/sergey/weatherapp/main/MainActivityPresenter.java
+++ app/src/main/java/com/example/sergey/weatherapp/main/MainActivityPresenter.java
@@ -2,6 +2,21 @@ package com.example.sergey.weatherapp.main;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.sergey.weatherapp.entities.Weather;
+import com.example.sergey.weatherapp.utilities.WeatherUtilites;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Date;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by Sergey on 9/14/2017.
@@ -15,4 +30,6 @@ public class MainActivityPresenter {
         this.mCtx = mCtx;
         this.mApi = mApi;
     }
+
+
 }
