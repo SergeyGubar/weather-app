14b3325b8b95db47ae5c3f32645893e45b9fceea
Sergey <sergey.gubar187@gmail.com>
2017-09-25 14:27:09 +0300

    Weird bug fix. Stupid mistake

--- app/src/main/java/com/example/sergey/weatherapp/utilities/IconUtils.java
+++ app/src/main/java/com/example/sergey/weatherapp/utilities/IconUtils.java
@@ -1,11 +1,12 @@
 package com.example.sergey.weatherapp.utilities;
 
+import android.app.ActivityManager;
+import android.util.Log;
+
 import com.example.sergey.weatherapp.helpers.IconHelper;
 import com.example.sergey.weatherapp.R;
 
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Map;
 
 /**
@@ -15,11 +16,14 @@ import java.util.Map;
 public class IconUtils implements IconHelper {
 
     private static Map<String, Integer> icons = createHash();
+    private static final String TAG = IconUtils.class.getSimpleName();
 
     private static Map<String, Integer> createHash() {
         Map<String, Integer> hash = new HashMap<>();
         hash.put("clear-day", R.drawable.ic_clear_day);
         hash.put("clear-night", R.drawable.ic_clear_night);
+        hash.put("partly-cloudy-day", R.drawable.ic_clear_day);
+        hash.put("partly-cloudy-night", R.drawable.ic_clear_night);
         hash.put("rain", R.drawable.ic_rain);
         hash.put("snow", R.drawable.ic_snow);
         hash.put("sleet", R.drawable.ic_rain);
@@ -30,6 +34,11 @@ public class IconUtils implements IconHelper {
 
     @Override
     public int getWeatherIcon(String iconName) {
-        return icons.get(iconName);
+        if (icons.containsKey(iconName)) {
+            return icons.get(iconName);
+        } else {
+            Log.e(TAG, "NPE BOYZ " + iconName);
+            return 0;
+        }
     }
 }
